c0f9c17680dad7873876f34252e52e0246673134
nick <hanxingchen1986@gmail.com>
2023-08-15 16:42:29 +0800

    对数据库，redis进行配置读取

--- 5/talkroom/server/apps/game_config.js
+++ 5/talkroom/server/apps/game_config.js
@@ -10,7 +10,7 @@ var game_config = {
 		0: {
 			stype: Stype.TalkRoom,
 			host: "127.0.0.1",
-			port: 6084, 
+			port: 6084,
 		},
 
 		1: {
@@ -20,6 +20,49 @@ var game_config = {
 		},
 
 	},
+
+	//数据库相关的配置
+	databases: {
+		//游戏的相关的数据库
+		game_database: {
+			host: "127.0.0.1",
+			port: 3306,
+			db_name: "bycw_game_node",
+			uname: "root",
+			upwd: "sanzhixiong",
+		},
+		//中心服务器
+		center_database: {
+			host: "127.0.0.1",
+			port: 3306,
+			db_name: "bycw_center_node",
+			uname: "root",
+			upwd: "sanzhixiong",
+		},
+
+		//redis的操作
+		center_redis: {
+			host: "127.0.0.1",
+			port: 6379,
+			db_index: 0,
+		},
+
+		game_redis: {
+			host: "127.0.0.1",
+			port: 6379,
+			db_index: 1,
+		},
+	},
+
+	//游戏注册的时候一些基础数据
+	game_data: {
+		first_uexp: 1000,
+		fitst_uchip: 1000,
+		login_bonuse_config: {
+			clear_login_straight: false,//是否需要清理
+			bonues: [100, 200, 300, 400, 500]
+		}
+	}
 };
 
 module.exports = game_config;
