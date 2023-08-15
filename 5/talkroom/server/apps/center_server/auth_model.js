db3aac5ba6b8f979bf5d5d7a4e16ef82bf1999fb
nick <hanxingchen1986@gmail.com>
2023-08-15 17:23:40 +0800

    第一个可以跑三个服务器的版本1.0

---
+++ 5/talkroom/server/apps/center_server/auth_model.js
@@ -0,0 +1,20 @@
+/**
+ * @Author: Nick
+ * @Date: 2023/8/15 17:21:14
+ * @LastEditors: Your Name
+ * @LastEditTime: 2023/8/15 17:21:14
+ * Description: 
+ * Copyright: Copyright (©)}) 2023 Your Name. All rights reserved.
+ * 登陆的相关数据部分
+ */
+
+var Respones = require("../Respones.js");
+var mysql_center = require("../../database/mysql_center.js");
+var redis_center = require("../../database/redis_center.js");
+var utils = require("../../utils/utils.js");
+var log = require("../../utils/log.js");
+var phone_msg = require("../phone_msg.js");
+
+module.exports = {
+
+};
